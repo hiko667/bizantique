2317c246c95a563ad6046ce1090246ce61d03dcc
hiko667 <kuleszastanislaw7547@gmail.com>
2026-08-05 09:51:36 +0200

    deploying in moment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Menu from './components/Menu';
 import MapPage from './pages/MapPage';
 import DataBasePage from './pages/DataBasePage';
@@ -11,7 +11,7 @@ export default function App() {
 
     return (
     <div className='overflow-hidden bg-dark text-light d-flex flex-column' style={{width : "100vw", height : "100vh"}}>
-        <BrowserRouter>
+        <HashRouter>
                 <Menu/>
                 <Routes>
                     <Route path = "*" element = {<Navigate to="/map" replace/>}/>
@@ -20,9 +20,7 @@ export default function App() {
                     {/* <Route path = "/data" element = {<DataBasePage/>}/> */}
                     <Route path = "/races/" element = {<RacesPage/>}/>
                 </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </div>
     )
 }
-
-
